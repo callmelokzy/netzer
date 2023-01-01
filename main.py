3b30a350b5ffe9e3840b2ca8a7e747a676ffc060
ivishnujs <ivishnujs@gmail.com>
2023-01-01 15:45:57 +0530

    first commit

---
+++ main.py
@@ -0,0 +1,45 @@
+import sys
+import time
+import socket
+import urllib.request
+import urllib.error
+
+class Netzer:
+
+    def __init__(self):
+        for i in range(25):
+          # Print a loading message and the current progress
+          message = "Checking Connection..." + "." * (i % 4)
+          print(f"\r{message}", end="")
+          # Sleep for a short amount of time to create the animation effect
+          time.sleep(0.2)
+        # Print a newline character to move the cursor to the next line
+        print("Done ✅")
+    def internet_private(self):
+
+        ipaddr = socket.gethostbyname(socket.gethostname())
+        if ipaddr == "127.0.0.1":
+            return False
+        else:
+            return ipaddr
+
+    def internet_public(self):
+        try:
+            check = urllib.request.urlopen('http://www.google.com', timeout=1)
+            public_ip = urllib.request.urlopen('https://ident.me').read().decode('utf8')
+            return public_ip
+        except urllib.error.URLError:
+            return False
+
+
+if __name__ == "__main__":
+
+    netzer = Netzer()
+    pvt_check = netzer.internet_private()
+    pub_check = netzer.internet_public()
+    print('Connected to Internet') if pvt_check and pub_check else print('No Internet')
+    time.sleep(0.2)
+    print('Public IPadrress: ', pub_check)
+    time.sleep(0.2)
+    print('Private IPaddress: ', pvt_check)
+
