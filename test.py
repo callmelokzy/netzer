6953d00c440f754e139d10c322b07abf4e504d03
callmelokzy <callmelokzy@gmail.com>
2023-01-23 10:31:38 +0530

    .

--- test.py
+++
@@ -1,7 +0,0 @@
-def print_box(text):
-    text_length = len(text)
-    print('+' + '-'*(text_length+2) + '+')
-    print('| ' + text + ' |')
-    print('+' + '-'*(text_length+2) + '+')
-
-print_box("Hello World")
